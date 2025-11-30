a5393b64153abf9ceaaa52b4b821454591f8d931
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-11-30 11:15:23 -0300

    Delete src/routes/resetSenhaRoutes.js

--- src/routes/resetSenhaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ResetSenhaController = require("../controllers/resetSenhaController");
-
-router.post("/enviar-codigo", ResetSenhaController.enviarCodigo);
-router.post("/verificar-codigo", ResetSenhaController.verificarCodigo);
-router.post("/nova-senha", ResetSenhaController.novaSenha);
-
-module.exports = router;
